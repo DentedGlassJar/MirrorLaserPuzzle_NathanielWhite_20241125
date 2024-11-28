cba70a7d6600efb87564b1352b1ed6609a68e984
DentedGlassJar <nathanielwhite867991@gmail.com>
2024-11-27 22:34:18 -0400

    Vector Draw Line Begun

--- Assets/Scripts/VectorScript.cs
+++ Assets/Scripts/VectorScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class VectorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject laserPointerObj;
+    public GameObject goalObj;
+
+    Vector2 mousePosition;
+    
+
 
     // Update is called once per frame
     void Update()
     {
+        OnDrawGizmos();
+    }
+
+    private void OnDrawGizmos()
+    {
+        mousePosition.y = Input.GetAxis("Vertical");
         
+        Vector2 laserStart = laserPointerObj.transform.position;
+        Vector2 laserEnd = mousePosition;
+
+        Debug.DrawLine(laserStart, laserEnd.normalized, Color.red);
+
+        Debug.Log($"laserEnd is {laserEnd}!");
     }
 }
